2634b7c9c2c89bff362938bc06a543379e32f2b7
roopeak <roopeak@protonmail.com>
2024-04-11 12:14:08 +0300

    Move parsing data to WeatherParser

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/WeatherData.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/WeatherData.java
@@ -5,18 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeatherData implements iAPI {
+    private WeatherParser weatherParser;
+    
+    public WeatherData() {
+        this.weatherParser = new WeatherParser();
+    }
+    
     private static final String API_KEY = "70cbfe7c86c4f2fcdd9fb493ace70183"; // Replace with your actual API key
     private static final String GEOLOCATION_API = "http://api.openweathermap.org/geo/1.0/direct";
     private static final String CURRENT_WEATHER_API = "http://api.openweathermap.org/data/2.5/weather";
     private static final String FORECAST_API = "http://api.openweathermap.org/data/2.5/forecast";
     
     @Override
-    public String lookUpLocation(String loc) {
+    public ArrayList<String> lookUpLocation(String loc) {
         try {
             String url = String.format("%s?q=%s&limit=1&appid=%s", GEOLOCATION_API, loc, API_KEY);
-            return fetchDataFromAPI(url);
+            String rawData = fetchDataFromAPI(url);
+            return weatherParser.parseLookUpLocation(rawData);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -24,10 +33,11 @@ public class WeatherData implements iAPI {
     }
     
     @Override
-    public String getCurrentWeather(double lat, double lon) {
+    public Weather getCurrentWeather(double lat, double lon) {
         try {
             String url = String.format("%s?lat=%s&lon=%s&appid=%s", CURRENT_WEATHER_API, lat, lon, API_KEY);
-            return fetchDataFromAPI(url);
+            String rawData = fetchDataFromAPI(url);
+            return weatherParser.parseCurrentWeather(rawData);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -35,17 +45,17 @@ public class WeatherData implements iAPI {
     }
 
     @Override
-    public String getForecast(double lat, double lon) {
+    public List<Weather> getForecast(double lat, double lon) {
         try {
             String url = String.format("%s?lat=%s&lon=%s&appid=%s", FORECAST_API, lat, lon, API_KEY);
-            return fetchDataFromAPI(url);
+            String rawData = fetchDataFromAPI(url);
+            return weatherParser.parseForecast(rawData);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    
     private String fetchDataFromAPI(String apiUrl) throws IOException {
         URL url = new URL(apiUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -60,7 +70,7 @@ public class WeatherData implements iAPI {
         } 
         in.close();
         connection.disconnect();
-        System.out.println(response.toString());
+
         return response.toString();
     }
 }

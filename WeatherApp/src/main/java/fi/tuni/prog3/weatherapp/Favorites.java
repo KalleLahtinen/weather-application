a006a18fff60676ec71417bd85a783491a153461
roopeak <roopeak@protonmail.com>
2024-04-18 16:02:20 +0300

    addFavoriteCity() and removeFavoriteCity()

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/Favorites.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/Favorites.java
@@ -1,7 +1,9 @@
 package fi.tuni.prog3.weatherapp;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +16,20 @@ public class Favorites implements iReadAndWriteToFile {
     private List<City> favoriteCities;
     
     public Favorites() {
-        favoriteCities = new ArrayList<>();
+        favoriteCities = new ArrayList<>(10);
     }
     
-    public void addFavoriteCity(City city) {
+    public boolean addFavoriteCity(City city) {
+        if (favoriteCities.contains(city)) {
+            return false;
+        }
+        
+        if (favoriteCities.size() == 10) {
+            return false;
+        }
+
         favoriteCities.add(city);
+        return true;
     }
     
     public void removeFavoriteCity(City city) {
@@ -30,7 +41,7 @@ public class Favorites implements iReadAndWriteToFile {
     }
     
     @Override
-    public readFromFile(String fileName) throws Exception {
+    public void readFromFile(String fileName) throws Exception {
         try (BufferedReader reader = new BufferedReader(
                                      new FileReader(fileName))) {
             String line;
@@ -39,6 +50,21 @@ public class Favorites implements iReadAndWriteToFile {
             }
         } catch (IOException e) {
             System.err.println("Error reading favorites from file: " + e.getMessage());
+        }
+    }
+    
+    @Override
+    public boolean writeToFile(String fileName) throws Exception {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (int i = 0; i < favoriteCities.size(); i++) {
+                writer.write(favoriteCities.get(i).getCity());
+                writer.newLine();
+            }
+            
+            return true;
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+            return false;
         }
     }
 }

dfec5660d8e0efcdbfcc05ea072d8635275206c2
Kalle Lahtinen <kalle.2.lahtinen@tuni.fi>
2024-04-22 23:13:43 +0300

    Add javadoc comments

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/ApplicationStateManager.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/ApplicationStateManager.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Manages the application state including measurement units, current city, 
+ * and lists of favorite cities and city search history.
+ * 
  * @author Roope Kärkkäinen
  */
 public class ApplicationStateManager {
@@ -13,6 +15,14 @@ public class ApplicationStateManager {
     public List<String> history;
     public List<String> favouriteCities;
 
+    /**
+     * Constructs an ApplicationStateManager with specified initial values.
+     * 
+     * @param units The initial measurement units.
+     * @param currentTown The initial city to be set as the current city.
+     * @param history The initial list of visited cities.
+     * @param favourites The initial list of favorite cities.
+     */
     public ApplicationStateManager(String units, String currentTown, List<String> history, List<String> favourites) {
         this.units = units;
         this.currentCity = currentTown;
@@ -20,6 +30,11 @@ public class ApplicationStateManager {
         this.favouriteCities = favourites;
     }
     
+    /**
+     * Constructs an ApplicationStateManager with default values.
+     * Default units are "metric", default city is "Helsinki", and both history
+     * and favorite cities lists are initialized as empty lists.
+     */
     public ApplicationStateManager() {
         this.units = "metric";
         this.currentCity = "Helsinki";
@@ -63,7 +78,7 @@ public class ApplicationStateManager {
     
     
     /**
-     * Method to add a city to the history of visited cities.
+     * Method to add a city to the search history.
      * @param city The city to add to history.
      */
     public void addCityToHistory(String city) {
@@ -81,46 +96,75 @@ public class ApplicationStateManager {
     }
     
     /**
-     * Method to remove a city from the history of visited cities.
+     * Method to remove a city from the search history.
      * @param city The city to remove from history.
      * @return True if the city is removed successfully, false if it doesn't exist in history.
      */
     public boolean removeCityFromHistory(String city) {
         return history.remove(city);
     }
-
     
-    // Getters and setters for each field
+    /**
+    * Provides the current measurement units.
+    * @return the current measurement units used in the application, such as "metric" or "imperial".
+    */
     public String getUnits() {
         return units;
     }
-    
+
+    /**
+     * Sets the measurement units.
+     * @param units the measurement units to set, such as "metric" or "imperial".
+     */
     public void setUnits(String units) {
         this.units = units;
     }
 
+    /**
+     * Retrieves the currently selected city.
+     * @return the current city.
+     */
     public String getCurrentCity() {
         return currentCity;
     }
 
+    /**
+     * Sets the currently selected city.
+     * @param currentCity the city to set as the current city.
+     */
     public void setCurrentCity(String currentCity) {
         this.currentCity = currentCity;
     }
 
+    /**
+     * Gets the search history list.
+     * @return a list of cities representing the user's search history.
+     */
     public List<String> getHistory() {
         return history;
     }
 
+    /**
+     * Sets the search history list.
+     * @param history a list of cities to set as the search history.
+     */
     public void setHistory(List<String> history) {
         this.history = history;
     }
 
+    /**
+     * Retrieves the list of favorite cities.
+     * @return a list of cities marked as favorites by the user.
+     */
     public List<String> getFavourites() {
         return favouriteCities;
     }
 
+    /**
+     * Sets the list of favorite cities.
+     * @param favourites a list of cities to be marked as favorites.
+     */
     public void setFavourites(List<String> favourites) {
         this.favouriteCities = favourites;
     }
-    
 }

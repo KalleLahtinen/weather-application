74c60bcf75af3d2c7d87b585c929055c6cf254ac
Kalle Lahtinen <kalle.2.lahtinen@tuni.fi>
2024-04-23 18:30:34 +0300

    Implement weather icons to hourly section of forecast view

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/ForecastViewDailySection.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/ForecastViewDailySection.java
@@ -5,10 +5,13 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 /*
     ChatGPT 3.5 was heavily utilized in this class to brainstorm possible 
@@ -42,9 +45,10 @@ public class ForecastViewDailySection {
         daysBox.getStyleClass().add("days-box");
         forecastView.setupDailyWeatherBindings(); // Initialize bindings
         
+        // Populate daysBox with individual days
         for (int i = 0; i < forecastView.displayedDays.size(); i++) {
             ObjectProperty<DailyWeather> weatherProp = forecastView.displayedDays.get(i);
-            VBox dayBox = new VBox(10);
+            VBox dayBox = new VBox();
             dayBox.setAlignment(Pos.CENTER);
             dayBox.getStyleClass().add("day-box");
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E dd.MM").
@@ -56,6 +60,11 @@ public class ForecastViewDailySection {
                 formatter.format(weatherProp.get().getDate().atZone(ZoneId.systemDefault())),
                 weatherProp));
             
+            Text dailyWeatherIcon = new Text();
+            dailyWeatherIcon.setFont(new Font("Weather Icons", 40));
+            dailyWeatherIcon.textProperty().bind(Bindings.format("%s",
+                    Bindings.selectString(weatherProp, "iconCode")));
+            
             Label tempRange = new Label();
             tempRange.getStyleClass().add("temp-range");
             tempRange.textProperty().bind(Bindings.format("%.0f%s ... %.0f%s",
@@ -63,8 +72,10 @@ public class ForecastViewDailySection {
                 Bindings.selectString(forecastView.measurementSystem, "tempUnit"),
                 Bindings.selectDouble(weatherProp, "maxTemp"),
                 Bindings.selectString(forecastView.measurementSystem, "tempUnit")));
+            VBox.setMargin(tempRange, new Insets(7, 0, 0, 0)); // Top, Right, Bottom, Left
+
             
-            dayBox.getChildren().addAll(dateLabel, tempRange);
+            dayBox.getChildren().addAll(dateLabel, dailyWeatherIcon, tempRange);
             daysBox.getChildren().add(dayBox);
 
             final int index = i;
